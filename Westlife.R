feceab86275bc2242d5e0cb76c98d3f893911e70
Azlin Rusnan <148675931+AzlinRusnan@users.noreply.github.com>
2024-05-02 21:43:01 +0800

    Update Westlife.R

--- Westlife.R
+++ Westlife.R
@@ -67,6 +67,44 @@ lyrics_df <- data.frame(
   stringsAsFactors = FALSE
 )
 
+#extracting the lyrics to csv file
 write.csv(lyrics_df, "My_Love_Lyrics.csv", row.names = FALSE)
+
+## Load the data
 my_love <- read.csv(file.choose())
 head(my_love)
+
+#1. Data Cleaning/Preprocessing:
+library(NLP)
+library(tm)  # for text mining
+library(SnowballC)  # for stopwords
+
+# Create a corpus
+corpus <- Corpus(VectorSource(my_love$Lyric))
+
+# Data cleaning
+corpus <- tm_map(corpus, content_transformer(tolower))
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, removeWords, stopwords("en"))
+corpus <- tm_map(corpus, stripWhitespace)
+
+# View cleaned data
+inspect(corpus)
+
+#2. Document Term Matrix and Frequency Tabulation:
+#Convert the text data to a document term matrix and find the frequency of terms
+dtm <- DocumentTermMatrix(corpus)
+freq <- colSums(as.matrix(dtm))
+freq <- sort(freq, decreasing = TRUE)
+top_freq <- head(freq, 5)  # adjust number for more terms
+
+# Display top frequencies
+print(top_freq)
+
+#3. Word Cloud:
+#Create a word cloud in any shape you prefer.
+library(RColorBrewer)
+library(wordcloud)
+
+# Generate a word cloud
+wordcloud(names(freq), freq, scale=c(3,0.5), max.words=100, random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "Dark2"))
